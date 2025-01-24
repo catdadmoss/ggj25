151ec4a622be2b2beb720a6ed6473e2fd3773bf9
Sachi <43549644+catdadmoss@users.noreply.github.com>
2025-01-24 21:41:35 +0100

    sticky mechanism

---
+++ GGJ25/Assets/Scripts/CollectorController.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class CollectorController : MonoBehaviour
+{
+    private float size = 1f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        size = transform.localScale.magnitude;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Collectible"))
+        {
+            if (collision.transform.localScale.magnitude <= size)
+            {
+                collision.transform.parent = transform;
+                size += collision.transform.localScale.magnitude;
+
+            }
+        }
+    }
+}
